0ef1b669632df28ef31a87a4f2886039237c2279
Kshitij Kasabekar <kshitijkasabekar007@gmail.com>
2026-01-01 19:07:35 +0530

    feat: convert RegisterPage to StatefulWidget for state management

--- lib/screens/register_page.dart
+++ lib/screens/register_page.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class RegisterPage extends StatelessWidget {
+class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
 
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
