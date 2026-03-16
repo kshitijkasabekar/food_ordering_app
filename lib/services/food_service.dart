47d68d0eb8e5e05d95d99454979dc46f5276c4ca
Kshitij Kasabekar <kshitijkasabekar007@gmail.com>
2026-03-16 22:38:23 +0530

    feat: enhance FoodService to include authorization header for fetching food items

--- lib/services/food_service.dart
+++ lib/services/food_service.dart
@@ -1,14 +1,25 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../models/food_item.dart';
+import 'token_service.dart';
 
 class FoodService {
   static const String baseUrl = 'http://10.0.2.2:8000';
 
+  final TokenService _tokenService = TokenService();
+
   Future<List<FoodItem>> fetchFoodItems() async {
+    final token = await _tokenService.getAccessToken();
+
     final url = Uri.parse('$baseUrl/food-items/');
 
-    final response = await http.get(url);
+    final response = await http.get(
+      url,
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer $token",
+      },
+    );
 
     if (response.statusCode == 200) {
       final data = jsonDecode(response.body);
@@ -17,7 +28,7 @@ class FoodService {
 
       return results.map((item) => FoodItem.fromJson(item)).toList();
     } else {
-      throw Exception('Failed to load food items');
+      throw Exception("Failed to load foods");
     }
   }
 }
