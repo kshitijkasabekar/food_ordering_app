f009b47d0fa558649d2ab34f3ec3f8897934425f
Kshitij Kasabekar <kshitijkasabekar007@gmail.com>
2026-03-24 20:47:21 +0530

    feat: add factory constructor for CartItem to create instances from JSON

--- lib/models/cart_item.dart
+++ lib/models/cart_item.dart
@@ -8,4 +8,11 @@ class CartItem {
     required this.food,
     this.quantity = 1,
   });
+
+  factory CartItem.fromJson(Map<String, dynamic> json) {
+    return CartItem(
+      food: FoodItem.fromJson(json['food']),
+      quantity: json['quantity'],
+    );
+  }
 }
