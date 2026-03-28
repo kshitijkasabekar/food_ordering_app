af7f85459a77e04875efc65180eabed2a4173066
Kshitij Kasabekar <kshitijkasabekar007@gmail.com>
2026-03-28 19:07:18 +0530

    feat: implement cart item display and handle empty cart state in CartPage

--- lib/screens/cart_page.dart
+++ lib/screens/cart_page.dart
@@ -1,17 +1,47 @@
 import 'package:flutter/material.dart';
+import '../services/cart_service.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+
+    final cartService = CartService();
+    final cartItems = cartService.cartItems;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Your Cart"),
       ),
-      body: const Center(
-        child: Text("Cart is empty"),
-      ),
+
+      body: cartItems.isEmpty
+          ? const Center(
+              child: Text("Cart is empty"),
+            )
+          : ListView.builder(
+              itemCount: cartItems.length,
+              itemBuilder: (context, index) {
+
+                final item = cartItems[index];
+                final food = item.food;
+
+                return ListTile(
+                  title: Text(food.name),
+
+                  subtitle: Text(
+                    "Qty: ${item.quantity} • ₹${food.price}",
+                  ),
+
+                  trailing: Text(
+                    "₹${food.price * item.quantity}",
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                );
+              },
+            ),
     );
   }
 }
