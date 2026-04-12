f902abb64879a6aad08f23db206ff6f223d029c9
Kshitij Kasabekar <kshitijkasabekar007@gmail.com>
2026-04-12 13:02:50 +0530

    feat: implement OrderService to handle order placement with authentication

---
+++ lib/services/order_service.dart
@@ -0,0 +1,31 @@
+// import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'token_service.dart';
+
+class OrderService {
+  static const String baseUrl = 'http://10.0.2.2:8000';
+
+  final TokenService _tokenService = TokenService();
+
+  Future<void> placeOrder() async {
+    final token = await _tokenService.getAccessToken();
+
+    if (token == null) {
+      throw Exception("User not authenticated");
+    }
+
+    final url = Uri.parse('$baseUrl/orders/');
+
+    final response = await http.post(
+      url,
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token',
+      },
+    );
+
+    if (response.statusCode != 200 && response.statusCode != 201) {
+      throw Exception('Failed to place order: ${response.body}');
+    }
+  }
+}
